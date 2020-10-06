ea21c13bc88946faf5777035fd0916004ff7020a
Joubran Almaarry <gbnooooomj@gmail.com>
2020-10-07 01:22:12 +0300

    program about deep copying

--- Prog_once.cpp
+++ Prog_once.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include <string>
+#include <memory>
 //#include"Log.h"
 
-class Entity {
+class Entity
+{
+private:
+	char* m_buffer;
+	unsigned int m_size;
 public:
-	Entity() {
-		std::cout << "created ENtity!" << std::endl;
+	Entity(const char* string) {
+		m_size = strlen(string);
+		m_buffer = new char[m_size + 1];
+		memcpy(m_buffer, string, m_size);
+		m_buffer[m_size] = 0;
+	}
+
+	Entity(const Entity& other)
+		:m_size(other.m_size)
+	{
+		m_buffer = new char[m_size + 1];
+		memcpy(m_buffer, other.m_buffer, m_size + 1);
+
 	}
 	~Entity() {
-		std::cout << "destroied Entity!" << std::endl;
+		delete[] m_buffer;
 	}
-};
 
-class scopedptr {
-private:
-	Entity* m_ptr;
-public:
-	scopedptr(Entity* ptr)
-		:m_ptr(ptr)
-	{
+	char& operator[](unsigned int index) {
+		return m_buffer[index];
 	}
-	~scopedptr() {
-		delete m_ptr;
-	}
+
+	friend std::ostream& operator<< (std::ostream& stream, const Entity en);
 };
+	std::ostream& operator<< (std::ostream & stream, const Entity en) {
+		std::cout << en.m_buffer << std::endl;
+		return stream;
+	}
 
 int main()
 {
-	{
-		scopedptr e = new Entity();
-	}
+	Entity obj = "joubran";
+	Entity obj2 = obj;
+	std::cout << obj << std::endl;
+	std::cout << obj2 << std::endl;
+	obj2[3] = 'z';
+
+
+
 
 	std::cin.get();
 }

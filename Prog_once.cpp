bd6aeeffacbdea4090f2d906d095d78adf9a16fe
Joubran Almaarry <gbnooooomj@gmail.com>
2020-10-07 02:22:05 +0300

    Automate deleting heap allocaed entity

--- Prog_once.cpp
+++ Prog_once.cpp
@@ -5,49 +5,30 @@
 
 class Entity
 {
-private:
-	char* m_buffer;
-	unsigned int m_size;
 public:
-	Entity(const char* string) {
-		m_size = strlen(string);
-		m_buffer = new char[m_size + 1];
-		memcpy(m_buffer, string, m_size);
-		m_buffer[m_size] = 0;
-	}
-
-	Entity(const Entity& other)
-		:m_size(other.m_size)
-	{
-		m_buffer = new char[m_size + 1];
-		memcpy(m_buffer, other.m_buffer, m_size + 1);
-
-	}
-	~Entity() {
-		delete[] m_buffer;
-	}
-
-	char& operator[](unsigned int index) {
-		return m_buffer[index];
-	}
-
-	friend std::ostream& operator<< (std::ostream& stream, const Entity en);
+	void print()const { std::cout << "Hello!" << std::endl; }
 };
-	std::ostream& operator<< (std::ostream & stream, const Entity en) {
-		std::cout << en.m_buffer << std::endl;
-		return stream;
+
+class scoped
+{
+private:
+	Entity* m_obj;
+public:
+	scoped(Entity* entity)
+		:m_obj(entity)
+	{}
+	~scoped() {
+		delete m_obj;
 	}
+	Entity* operator->() {
+		return m_obj;
+	}
+};
 
 int main()
 {
-	Entity obj = "joubran";
-	Entity obj2 = obj;
-	std::cout << obj << std::endl;
-	std::cout << obj2 << std::endl;
-	obj2[3] = 'z';
-
-
-
+	scoped obj = new Entity();
+	obj->print();
 
 	std::cin.get();
 }

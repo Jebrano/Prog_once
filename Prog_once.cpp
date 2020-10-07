36a9d21753e03ebc756136c85615950646563e2f
Joubran Almaarry <gbnooooomj@gmail.com>
2020-10-07 03:11:23 +0300

    Using vectors

--- Prog_once.cpp
+++ Prog_once.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <string>
-#include <memory>
-//#include"Log.h"
+#include <vector>
 
-struct Vector
+struct matrix
 {
 	float x, y, z;
 };
 
+std::ostream& operator<< (std::ostream& stream, const matrix& entity) {
+	stream << entity.x << ", " << entity.y << ", " << entity.z << ", ";
+	return stream;
+}
+
+void function(const std::vector<matrix> matat) {
+
+}
+
 int main()
 {
-	int offset = (int)&((Vector*)nullptr)->y;
-	std::cout << offset << std::endl;
+	std::vector<matrix> matrixes;
+	matrixes.push_back({ 1, 2, 3 });
+	matrixes.push_back({ 4, 5, 6 });
+	function(matrixes);
+
+	for (int i = 0; i < matrixes.size(); i++)
+	{
+		std::cout << matrixes[i] << std::endl;
+	}
+
+	matrixes.erase(matrixes.end() - 1);
+
+	for (matrix& m : matrixes) {
+		std::cout << m << std::endl;
+	}
+
 
 	std::cin.get();
 }

5f033fa8546a6dd64947a520b57a94f2c168166c
Joubran Almaarry <gbnooooomj@gmail.com>
2020-10-10 01:27:34 +0300

    Makint a simple template

--- Prog_once.cpp
+++ Prog_once.cpp
@@ -2,28 +2,17 @@
 #include <string>
 #include <vector>
 
-struct matrix
-{
-	float m_x, m_y, m_z;
-
-	matrix(float x, float y, float z)
-		:m_x(x), m_y(y), m_z(z)
-	{
-	}
-	matrix(const matrix& matt)
-		:m_x(matt.m_x), m_y(matt.m_y), m_z(matt.m_z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
+template<typename T, int N>
+class Array {
+private:
+	T m_array[N];
+public:
+	T getsize() const { return N; }
 };
 
 int main()
 {
-	std::vector<matrix> matrixes;
-	matrixes.reserve(3);
-	matrixes.emplace_back(1, 2, 3);
-	matrixes.emplace_back(4, 5, 6);
-	matrixes.emplace_back(7, 8, 9);
-
+	Array<int, 5> obj;
+	std::cout << obj.getsize() << std::endl;
 	std::cin.get();
 }
